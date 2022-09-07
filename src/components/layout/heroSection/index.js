f8360f1be8689beafce7abf1c128c925eb00712c
roniceyemeli <roniceyemeli@gmail.com>
2022-09-07 11:51:22 +0000

    added first image feature in the here section

--- src/components/layout/heroSection/index.js
+++ src/components/layout/heroSection/index.js
@@ -13,13 +13,21 @@ const HeroSection = () => {
               one-stop-shop for spending, saving, budgeting, investing, and much
               more.
             </p>
-            <div className="btn"><button className="btn_request">Request Invite</button></div>
+            <div className="btn">
+              <a href="#buton">
+                <button className="btn_request">Request Invite</button>
+              </a>
+            </div>
           </div>
         </div>
         <div className="hero_right">
-            <div className="img">
-                <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480592/easyBank/bg-intro-desktop_kmmult.svg" alt="phones" />
-            </div>
+          <div className="img">
+            <img
+              src="https://res.cloudinary.com/ron2021/image/upload/v1662480592/easyBank/bg-intro-desktop_kmmult.svg"
+              alt="phones_backgound"
+            />
+            <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480592/easyBank/image-mockups_ynaj4f.png" alt="phones" />
+          </div>
         </div>
       </div>
     </div>
