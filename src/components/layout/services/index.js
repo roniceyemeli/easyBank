ca5caf9e9a2169e4b1d45a10e00a816ed4bd280c
roniceyemeli <roniceyemeli@gmail.com>
2022-09-08 14:11:39 +0000

    services section added

--- src/components/layout/services/index.js
+++ src/components/layout/services/index.js
@@ -13,6 +13,28 @@ const Services = () => {
           Control your finances like never before
         </p>
       </div>
+      <div className="services-boxes">
+        <div className="services-boxe">
+            <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480599/easyBank/icon-online_vl1x0p.svg" alt="cheque" />
+            <h3>Online Banking</h3>
+            <p>Our modern web and mobile <br/> applications allow you to keep <br/> track of your finances wherever you <br/> are in the world.</p>
+        </div>
+        <div className="services-boxe">
+            <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480593/easyBank/icon-budgeting_hhnrrs.svg" alt="money" />
+            <h3>Simple Budgeting</h3>
+            <p>See exactly where your money<br/> goes each month. Receive<br/> notifications when your're close to  <br/> hitting your limits.</p>
+        </div>
+        <div className="services-boxe">
+            <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480593/easyBank/icon-budgeting_hhnrrs.svg" alt="money" />
+            <h3>Fast Onboarding</h3>
+            <p>Our modern web and mobile <br/> applications allow you to keep <br/> track of your finances wherever you <br/> are in the world.</p>
+        </div>
+        <div className="services-boxe">
+            <img src="https://res.cloudinary.com/ron2021/image/upload/v1662480593/easyBank/icon-budgeting_hhnrrs.svg" alt="money" />
+            <h3>Open Api</h3>
+            <p>Manage your saving,investments, <br/> pension, and much more from one <br/> account. Tracking your money has <br/> never been easier.</p>
+        </div>
+      </div>
     </div>
   );
 };
