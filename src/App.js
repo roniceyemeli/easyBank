b36e1365bff5d104e3c243a6e4e4e783201f9c01
roniceyemeli <roniceyemeli@gmail.com>
2022-09-11 20:31:57 +0000

    added footer file in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
 import Layout from './components/layout';
 import NavBar from './components/navBar';
-// import Footer from './components/footer/';
+import Footer from './components/footer/';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Layout/>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './index.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div id='footer'>
+      <div className="main_footer">
+        <div className="footer_block">
+        <a href="#home"><img
+          src="https://res.cloudinary.com/ron2021/image/upload/v1662928129/easyBank/Group_sisxl2.svg"
+          alt="easy_bank_logo"
+        /></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
