757e7974d98c1f54cde22b29d7d496577bca8ef9
roniceyemeli <roniceyemeli@gmail.com>
2022-09-12 20:13:50 +0000

    footer architechture done

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,40 +6,38 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="main_footer">
-        <div className="footer_links">
-          <a href="#home">
-            <img
-              src="https://res.cloudinary.com/ron2021/image/upload/v1662928129/easyBank/Group_sisxl2.svg"
-              alt="easy_bank_logo"
-            />
-          </a>
-          <div className="social_media">
-            <Icons.GrFacebook />
-            <Icons.GrYoutube />
-            <Icons.GrTwitter />
-            <Icons.GrPinterest />
-            <Icons.GrInstagram />
-          </div>
-        </div>
-        <div className="footer_navlinks">
-            <div className="footer_navlinks_list">
-              <ul>
-                <li>About Us</li>
-                <li>Contact</li>
-                <li>Blog</li>
-              </ul>
-              <ul>
-                <li>Careers</li>
-                <li>Support</li>
-                <li>Privacy Policy</li>
-              </ul>
+        <div className="footer_left">
+          <div className="footer_links">
+            <a href="#home">
+              <img
+                src="https://res.cloudinary.com/ron2021/image/upload/v1662928129/easyBank/Group_sisxl2.svg"
+                alt="easy_bank_logo"
+              />
+            </a>
+            <div className="social_media">
+              <Icons.GrFacebook />
+              <Icons.GrYoutube />
+              <Icons.GrTwitter />
+              <Icons.GrPinterest />
+              <Icons.GrInstagram />
             </div>
+          </div>
+            <ul>
+              <li><a href="#home">About Us</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="#blog">Blog</a></li>
+            </ul>
+            <ul>
+              <li><a href="#careers">Careers</a></li>
+              <li><a href="#support">Support</a></li>
+              <li><a href="#privacyPolicy">Privacy Policy</a></li>
+            </ul>
         </div>
-        <div className="footer_details">
-        <a href="#button">
-          <button className="btn_request">request invite</button>
-        </a>
-        <p>Easybank. All Rights Reserved</p>
+        <div className="footer_right">
+          <a href="#button">
+            <button className="btn_request">request invite</button>
+          </a>
+          <p>Easybank. All Rights Reserved</p>
         </div>
       </div>
     </div>
