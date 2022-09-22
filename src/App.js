7a3ccf9ca89e69e62fd540483756615fead3afe1
roniceyemeli <roniceyemeli@gmail.com>
2022-09-22 16:21:24 +0000

    auto close event added for the menu responsive

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import './App.scss';
 import Layout from './components/layout';
 import NavBar from './components/navBar';
 import Footer from './components/footer/';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Menu from './components/menu';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+    window.addEventListener("resize", handleWindowResize);
+    if (windowSize.innerWidth >= 1020) {
+      setMenuOpen(false);
+    }
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [windowSize]);
+
+  function getWindowSize() {
+    if (typeof window !== "undefined") {
+      const { innerWidth, innerHeight } = window;
+      return { innerWidth, innerHeight };
+    }
+  }
   return (
     <div className="App">
       <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
